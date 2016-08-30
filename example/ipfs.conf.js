73971d1cf955d5c9fbe8af28bb4b53a27b98ed9b
haad <haad@headbanggames.com>
2016-08-30 23:01:57 +0200

    Proper roadmap conf file examples

---
+++ example/ipfs.conf.js
@@ -0,0 +1,56 @@
+'use strict'
+
+module.exports = {
+  // Name of the organization or project this roadmap is generated for
+  organization: 'IPFS',
+
+  // Include open and closed milestones where due date is after milestonesStartDate
+  milestonesStartDate: '2016-08-01T00:00:00Z', // ISO formatted timestamp
+
+  // Include open and closed milestones where due date is before milestonesEndDate
+  milestonesEndDate: '2016-11-01T00:00:00Z', // ISO formatted timestamp
+
+  // Github repository to open open a Pull Request with the generated roadmap
+  targetRepo: "ipfs/pm",
+
+  // List of projects that this roadmap covers
+  projects: [
+    {
+      name: "IPFS",
+      repos: [
+        "ipfs/ipfs",
+        "ipfs/pm",
+      ],
+    },
+    {
+      name: "go-ipfs",
+      repos: [
+        "ipfs/go-ipfs",
+      ],
+    },
+    {
+      name: "js-ipfs",
+      repos: [
+        "ipfs/js-ipfs",
+        "ipfs/js-ipfs-api",
+        "ipfs/interface-ipfs-core",
+      ],
+    },
+    {
+      name: "orbit",
+      repos: [
+        "haadcode/orbit",
+        "haadcode/orbit-db",
+        "haadcode/orbit-db-store",
+        "haadcode/orbit-db-kvstore",
+        "haadcode/orbit-db-eventstore",
+        "haadcode/orbit-db-feedstore",
+        "haadcode/orbit-db-counterstore",
+        "haadcode/orbit-crypto",
+        "haadcode/ipfs-log",
+        "haadcode/ipfs-post",
+        "haadcode/crdts",
+      ],
+    },
+  ]
+}
